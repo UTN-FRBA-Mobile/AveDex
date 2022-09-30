0f1d955ecc3c56306e57eacce0431d8d90d56b8c
MarcosInfantino <marcosinfantino698@gmail.com>
2022-09-29 22:32:57 -0300

    Camara

--- app/src/main/java/com/example/avedex/MainActivity.kt
+++ app/src/main/java/com/example/avedex/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.avedex
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -16,22 +19,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivity(takePictureIntent)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        dispatchTakePictureIntent()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
